47be3519376c1b9fabb412f296a32d4b80607458
Gilvam Mourão <gilvamm@gmail.com>
2019-07-08 15:37:18 -0300

    04 - Setup do projeto com Spring Initializr

---
+++ src/main/java/br/com/devdojo/DevDojoSpringBootEssentialsApplication.java
@@ -0,0 +1,13 @@
+package br.com.devdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevDojoSpringBootEssentialsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevDojoSpringBootEssentialsApplication.class, args);
+    }
+
+}
