f42593df9095be7d22bb9f9b802e860e8e5824cc
Gilvam Mourão <gilvamm@gmail.com>
2019-07-09 11:35:04 -0300

    08 - Adicionando Spring Data JPA com MySQL pt 01

--- src/main/java/br/com/devdojo/endpoint/StudentEndPoint.java
+++ src/main/java/br/com/devdojo/endpoint/StudentEndPoint.java
@@ -2,66 +2,61 @@ package br.com.devdojo.endpoint;
 
 import br.com.devdojo.error.CustomErrorType;
 import br.com.devdojo.model.Student;
-import br.com.devdojo.util.DateUtil;
+import br.com.devdojo.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.ws.Response;
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("students")
 public class StudentEndPoint {
 
-    private final DateUtil dateUtil;
+
+    private final StudentRepository studentDAO;
 
     @Autowired
-    public StudentEndPoint(DateUtil dateUtil) {
-        this.dateUtil = dateUtil;
+    public StudentEndPoint(StudentRepository studentDAO) {
+        this.studentDAO = studentDAO;
     }
 
     //@RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public ResponseEntity<?> listAll() {
-        System.out.println(dateUtil.formatLocalDateTimeToDataBaseStyle(LocalDateTime.now()));
-        return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
+        return new ResponseEntity<>(studentDAO.findAll(), HttpStatus.OK);
     }
 
     //@RequestMapping(method = RequestMethod.GET, path = "/{id}")
     @GetMapping(path = "/{id}")
-    public ResponseEntity<?> getStudentById(@PathVariable("id") int id) {
-        Student student = new Student();
-        student.setId(id);
-        int index = Student.getStudentList().indexOf(student);
-        if (index == -1) {
+    public ResponseEntity<?> getStudentById(@PathVariable("id") Long id) {
+
+        Optional<Student> student = this.studentDAO.findById(id);
+
+        if (student == null) {
             return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
+        return new ResponseEntity<>(student, HttpStatus.OK);
     }
 
     //@RequestMapping(method = RequestMethod.POST)
     @PostMapping
     public ResponseEntity<?> save(@RequestBody Student student) {
-        Student.studentList.add(student);
-        return new ResponseEntity<>(student, HttpStatus.OK);
+        return new ResponseEntity<>(this.studentDAO.save(student), HttpStatus.OK);
     }
 
     //@RequestMapping(method = RequestMethod.DELETE)
-    @DeleteMapping
-    public ResponseEntity<?> delete(@RequestBody Student student) {
-        Student.studentList.remove(student);
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        this.studentDAO.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     //@RequestMapping(method = RequestMethod.PUT)
     @PutMapping
     public ResponseEntity<?> update(@RequestBody Student student) {
-        Student.studentList.remove(student);
-        Student.studentList.add(student);
+        this.studentDAO.save(student);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
